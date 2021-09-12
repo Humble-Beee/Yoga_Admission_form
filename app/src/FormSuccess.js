b8a17477f60a8cc119ef5cea7f9bfaadc4d2f192
Neeraj Kumar <neeraj547266@gmail.com>
2021-09-12 05:39:27 +0530

    first commit

---
+++ app/src/FormSuccess.js
@@ -0,0 +1,16 @@
+  
+import React from 'react';
+import './Form.css';
+
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+     <div className='form-success'>We have received your request!</div>
+      
+      <img src='img/img-1.jpg.jpg' width="400" 
+     height="500" alt='success-image' className='form-img-2' />
+    </div>
+  );
+};
+
+export default FormSuccess;
